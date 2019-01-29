1c79ad6bd7a122821e1cd708d2d431e5e1aafc09
Grzegorz <kalarus.g@gmail.com>
2019-01-29 17:23:16 +0100

    Updates

---
+++ precache-manifest.e4c2f62fd69ed0fa99c7018bb9cd8ffe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "864c705ef7523a0ea63bb6bbe6224c85",
+    "url": "/sunday/static/media/img2.864c705e.jpg"
+  },
+  {
+    "revision": "bc1450a14fa20a7ea958975d981e4397",
+    "url": "/sunday/static/media/img1.bc1450a1.jpg"
+  },
+  {
+    "revision": "4fab21af642ba6aefd2b0024cdc55ee4",
+    "url": "/sunday/static/media/ico-3.4fab21af.svg"
+  },
+  {
+    "revision": "3399bb5ce1907d5fbbee92a71e5f09aa",
+    "url": "/sunday/static/media/ico-2.3399bb5c.svg"
+  },
+  {
+    "revision": "5face12ee8aeaec1f78681f81bca9fe9",
+    "url": "/sunday/static/media/ico-1.5face12e.svg"
+  },
+  {
+    "revision": "9f6f8bcf74171f5ddf45",
+    "url": "/sunday/static/js/runtime~main.9f6f8bcf.js"
+  },
+  {
+    "revision": "c2d19551472f37f36910",
+    "url": "/sunday/static/js/main.c2d19551.chunk.js"
+  },
+  {
+    "revision": "d2d3d3c796ce88c77ae3",
+    "url": "/sunday/static/js/1.d2d3d3c7.chunk.js"
+  },
+  {
+    "revision": "c2d19551472f37f36910",
+    "url": "/sunday/static/css/main.620cb2ae.chunk.css"
+  },
+  {
+    "revision": "02a99166eb7b972a0ede8ef462e22178",
+    "url": "/sunday/index.html"
+  }
+];
